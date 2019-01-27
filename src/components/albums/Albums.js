c0745b5a4d25f728a2a0553c2491dc0f46bae8c2
Jason van Schaik <jsonschaik@gmail.com>
2019-01-27 19:41:05 +0100

    fix

--- src/components/albums/Albums.js
+++ src/components/albums/Albums.js
@@ -8,6 +8,11 @@ class Albums extends Component {
         albums: [],
     };
 
+     refreshPage = async () => {
+        const res =  await axios.get('https://musicapi2018-2019.herokuapp.com/api/albums');
+        this.setState({albums: res.data.items});
+    };
+
     async componentDidMount() {
         const res = await axios.get('https://musicapi2018-2019.herokuapp.com/api/albums');
         this.setState({albums: res.data.items});
@@ -16,16 +21,17 @@ class Albums extends Component {
     render() {
         const albums = this.state.albums;
         return (
-            <React.Fragment >
+            <React.Fragment>
                 <h1 className="display-4 mb-2">
                     <span className="text-danger">Albums</span>
                 </h1>
                 {albums.map(album => (
-                    <Album key={album._id} album={album}/>
+                    <Album key={album._id} album={album} refreshPage = {this.refreshPage}/>
                 ))}
             </React.Fragment>
         )
     }
 }
+
 
 export default Albums;
